c6adf8c24bf0e8df669aae3784ebe380a471bd94
Ingrid <ingridespangler@gmail.com>
2021-03-07 19:09:25 -0300

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,35 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-                "\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
+
+        public String value(Customer aCustomer) {
+            Enumeration rentals = aCustomer.getRentals();
+            String result = formatName(aCustomer.getName());
+            while (rentals.hasMoreElements()) {
+                Rental each = (Rental) rentals.nextElement();
+                //show figures for each rental
+                result += formatRentalData(each);
+            }
+            //add footer lines
+            result += addFooter(aCustomer);
+            return result;
         }
-        //add footer lines
-        result += "Amount owed is " +
-                String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-                String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-                " frequent renter points";
-        return result;
-    }
+
+        private String formatName(String name){
+            return "Rental Record for " + name +
+                    "\n";
+        }
+
+        private String formatRentalData(Rental movie){
+            return "\t" + movie.getMovie().getTitle()+ "\t" +
+                    String.valueOf(movie.getCharge()) + "\n";
+        }
+
+        private String addFooter(Customer aCustomer){
+            return "Amount owed is " +
+                    String.valueOf(aCustomer.getTotalCharge()) + "\n"
+                    + "You earned " +
+                    String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+                    " frequent renter points";
+        }
 }
