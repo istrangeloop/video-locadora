353642396001b7fa637f31c85ab76f2e650eb827
Ingrid <ingridespangler@gmail.com>
2021-03-07 18:47:20 -0300

    Commit 16

--- Customer.java
+++ Customer.java
@@ -8,7 +8,9 @@ public class Customer {
    public Customer (String name){
       _name = name;
    }
-
+    public Enumeration getRentals() {
+        return _rentals.elements();
+    }
    public void addRental(Rental arg) {
       _rentals.addElement(arg);
    }
@@ -18,20 +20,7 @@ public class Customer {
    }
   
    public String statement() {
-     Enumeration rentals = _rentals.elements();
-     String result = "Rental Record for " + getName() + "\n";
-     while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-        //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(each.getCharge()) + "\n";
-
-     }
-     //add footer lines
-     result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-     result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-             " frequent renter points";
-     return result;
+       return new TextStatement().value(this);
    }
 
     public String htmlStatement() {
@@ -52,7 +41,7 @@ public class Customer {
         return result;
     }
 
-    private double getTotalCharge() {
+    public double getTotalCharge() {
         double result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
@@ -62,7 +51,7 @@ public class Customer {
         return result;
     }
 
-    private int getTotalFrequentRenterPoints(){
+    public int getTotalFrequentRenterPoints(){
         int result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
