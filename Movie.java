13d547273386983b3977302b510d0b574eb24388
Ingrid <ingridespangler@gmail.com>
2021-03-07 18:21:12 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -11,6 +11,28 @@ public class Movie {
       _title = title;
       _priceCode = priceCode;
   }
+    public double getCharge(int daysRented){
+        //Adicionar o trecho de código extraído.
+        //determine amounts for aRental line
+        double thisAmount = 0;
+
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 
   public int getPriceCode() {
       return _priceCode;
